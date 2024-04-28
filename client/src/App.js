a8f8650c98c3c0a63ec1e178833e7ab1b2351f48
Lenia Li <lenia17.lq@gmail.com>
2024-04-28 12:59:48 -0700

    Setup the basic file for forntend

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/utils/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const BASE_URL = process.env.API_BASE_URL || "http://localhost:5000";
+
+const Lensphere_API = {
+
+};
+
+export default Lensphere_API;
